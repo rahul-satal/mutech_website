0aa15365207c6c729e2069df67a3b801ee32e704
Shiv-Dangi <shiv.dangi71@gmail.com>
2015-03-30 17:07:38 +0530

    contact us User Interface designed

--- mutech/urls.py
+++ mutech/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from mutech import views
 
 urlpatterns = patterns('',
-    url(r'^mutech/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^mutech/header/$', views.header, name='header'),
     url(r'^mutech/footer/$', views.footer, name='footer'),
     url(r'^mutech/aboutus/$', views.aboutus, name='aboutus'),
